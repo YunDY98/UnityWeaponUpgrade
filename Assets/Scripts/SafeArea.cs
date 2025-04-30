8fbc8f1d8bf8be39603fd175b35c3f65e1fa37db
yun <dbsejrdid123@gmail.com>
2025-04-30 12:30:13 +0900

    인게임 플레이어 정보 safearea 적용(노치, 펀치홀 대응)

---
+++ Assets/Scripts/SafeArea.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform panel;
+
+    void Awake()
+    {
+        ApplySafeArea(panel);
+        
+    }
+    void ApplySafeArea(RectTransform panel)
+    {
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        panel.anchorMin = anchorMin;
+        panel.anchorMax = anchorMax;
+    }
+}
